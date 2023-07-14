247b65a7cea1b07ec20f3f3115180510d061f879
tw1ster4k <aminurshinov@gmail.com>
2023-07-14 15:57:17 +0500

    Release3.9.0

--- Tsunami/Components/Blocks/Navbar/Header.jsx
+++ Tsunami/Components/Blocks/Navbar/Header.jsx
@@ -5,6 +5,8 @@ import Title from '../../Svg/Title/Title';
 import {useSelector} from 'react-redux';
 import {stylesHeaderDark} from './StylesHeaderDark';
 import {stylesHeaderWhite} from './StylesHeaderWhite';
+import { SvgCssUri } from 'react-native-svg';
+import Logo from "../../Image/Logo.svg"
 
 const Header = () => {
   const theme = useSelector(state => state.theme);
@@ -25,7 +27,12 @@ const Header = () => {
           alignItems: 'center',
         },
       ]}>
-      <Title />
+      {/* <Title />  */}
+      {theme ?
+        <SvgCssUri style={{marginTop:48}} width="208" height="58" uri="https://vk.com/doc346636180_666069690?hash=YjTfjPZMTqHmogE7W9JauQ4afGpqB0oBwu1QHdZvd4X&dl=3jsZGKc4rojIhPnuaKQVOicGPtEJzFRbG347tLlDyVP" />
+        :
+      <SvgCssUri style={{marginTop:48}} width="208" height="58" uri="https://vk.com/doc346636180_666069486?hash=ggbwihP82kAjBTjhZ6He3nz6miZrJd2pwOd6xn7i7WL&dl=swGiTL5EDS1q2Wm7uH4WVtzKRznHLABRzQj7Znn7G20" />
+      }
       <Text
         style={[
           styles.text,
