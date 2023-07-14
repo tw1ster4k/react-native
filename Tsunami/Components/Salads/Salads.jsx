ceedfbd2afc0d63615a14c8c92ba060b806249bd
tw1ster4k <aminurshinov@gmail.com>
2023-07-14 13:21:40 +0500

    Release3.7.0

--- Tsunami/Components/Salads/Salads.jsx
+++ Tsunami/Components/Salads/Salads.jsx
@@ -7,17 +7,28 @@ import { useSelector } from 'react-redux'
 import True from '../Svg/True/True'
 import Card from '../Blocks/Card/Card'
 import { stylesSalads } from './stylesSalads'
+import { useState } from 'react'
+import CardSvg from '../Svg/CardSvg/CardSvg'
 
 
 const Salads = () => {
     const theme = useSelector((state) => state.theme)
     const salads = useSelector((state) => state.salads)
+    const [loading, setLoading] = useState(true)
     const styles = StyleSheet.create(theme ? stylesSaladsWhite : stylesSaladsDark)
     const styles2 = StyleSheet.create(stylesSalads)
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+
   return (
         <ScrollView style={[styles.container, styles2.container]}>
             <Text style={[styles.tab, styles2.tab]}>Салаты</Text>
             {  salads.map((el,index) =>
+            loading ?
+            <CardSvg key={index} />
+            :
               <Card data={el} key={index} index={index} />
             )
 
---
+++ Tsunami/Components/Svg/CardSvg/CardSvg.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Svg, Rect } from 'react-native-svg'
+import { useSelector } from 'react-redux'
+
+const CardSvg = () => {
+    const theme = useSelector((state) => state.theme)
+  return (
+    theme ?
+    <Svg
+    style={{marginTop:18,marginLeft:10,}}
+    xmlns="http://www.w3.org/2000/svg"
+    width={368}
+    height={118}
+    viewBox="0 0 368 118"
+    fill="none"
+  >
+    <Rect x={1} y={1} width={366} height={116} rx={7} fill="white" />
+    <Rect
+      x={1}
+      y={1}
+      width={366}
+      height={116}
+      rx={7}
+      stroke="#BBBBBB"
+      strokeWidth={2}
+    />
+  </Svg>
+    :
+    <Svg
+    style={{marginTop:18,marginLeft:10,}}
+    xmlns="http://www.w3.org/2000/svg"
+    width={368}
+    height={118}
+    viewBox="0 0 368 118"
+    fill="none"
+  >
+    <Rect x={1} y={1} width={366} height={116} rx={7} fill="#222222" />
+    <Rect
+      x={1}
+      y={1}
+      width={366}
+      height={116}
+      rx={7}
+      stroke="#555555"
+      strokeWidth={2}
+    />
+  </Svg>
+  )
+}
+
+export default CardSvg
